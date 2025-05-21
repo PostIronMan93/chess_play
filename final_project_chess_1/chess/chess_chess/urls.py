6ae991848fa2b0347338ce7e42c318281f50e581
PostIronMan93 <123867878+PostIronMan93@users.noreply.github.com>
2025-05-21 22:27:51 +0300

    Delete final_project_chess_1/chess directory

--- final_project_chess_1/chess/chess_chess/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('move_piece/', views.move_piece, name='move_piece'),
-    path('new_game/', views.new_game, name='new_game'),
-]
-
--- final_project_chess_1/chess/final_project_chess_1/chess_chess/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import ChessGame
-
-class ChessGameAdmin(admin.ModelAdmin):
-    list_display = ('id', 'turn', 'board_state', 'move_history')
-
-admin.site.register(ChessGame, ChessGameAdmin)
